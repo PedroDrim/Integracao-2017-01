19fcafa56be4817165e2b9c7cda3ffd35a8cdad8
pedrodrim <pedrohenriquedrim@gmail.com>
2017-06-07 19:58:40 -0300

    refatoração da arquitetura e implementação Util dos 11 primeiros padroes do codacy pt.2

--- Codigo-Fonte/implementacao/src/test/java/br/ufg/inf/horus/implementation/BsusTest.java
+++ Codigo-Fonte/implementacao/src/test/java/br/ufg/inf/horus/implementation/BsusTest.java
@@ -15,26 +15,16 @@ import static org.junit.Assert.assertEquals;
 
 /**
  *
- * @author aluno
+ * @author Vinicius
  */
 public class BsusTest {
 
-    private String xml;
+    private StringBuilder xml;
     private Bsus bsus;
 
     public BsusTest() {
         bsus = new Bsus();
-        bsus.setLog(new Log() {
-            @Override
-            public void info(String message) {
-                System.out.println(message);
-            }
-
-            @Override
-            public void erro(String message) {
-                System.err.println(message);
-            }
-        });
+        bsus.setLog(new TestLog());
     }
 
     /**
@@ -43,27 +33,27 @@ public class BsusTest {
      */
     @Before
     public void setUp() {
-        this.xml = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">\n"
-                + "   <soap:Header xmlns:est=\"http://servicos.saude.gov.br/horus/v1r0/EstoqueService\"/>\n"
-                + "   <soap:Body xmlns:est=\"http://servicos.saude.gov.br/horus/v1r0/EstoqueService\">\n"
-                + "      <soap:Fault>\n"
-                + "         <soap:Code>\n"
-                + "            <env:Value xmlns:env=\"http://www.w3.org/2003/05/soap-envelope\">env:Sender</env:Value>\n"
-                + "         </soap:Code>\n"
-                + "         <soap:Reason>\n"
-                + "            <soap:Text xml:lang=\"pt-BR\">Uma ou mais regras negociais foram violadas, verifique a lista de erros.</soap:Text>\n"
-                + "         </soap:Reason>\n"
-                + "         <soap:Detail>\n"
-                + "            <msf:MSFalha xmlns:msf=\"http://servicos.saude.gov.br/wsdl/mensageria/falha/v5r0/msfalha\">\n"
-                + "               <msf:Mensagem xmlns:men=\"http://servicos.saude.gov.br/wsdl/mensageria/falha/v5r0/mensagem\">\n"
-                + "                  <men:codigo>OSB_SEM_AUTENTICACAO</men:codigo>\n"
-                + "                  <men:descricao>As credenciais informadas não são válidas</men:descricao>\n"
-                + "               </msf:Mensagem>\n"
-                + "            </msf:MSFalha>\n"
-                + "         </soap:Detail>\n"
-                + "      </soap:Fault>\n"
-                + "   </soap:Body>\n"
-                + "</soap:Envelope>";
+        this.xml.append("<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">\n");
+        this.xml.append("   <soap:Header xmlns:est=\"http://servicos.saude.gov.br/horus/v1r0/EstoqueService\"/>\n");
+        this.xml.append("   <soap:Body xmlns:est=\"http://servicos.saude.gov.br/horus/v1r0/EstoqueService\">\n");
+        this.xml.append("      <soap:Fault>\n");
+        this.xml.append("         <soap:Code>\n");
+        this.xml.append("            <env:Value xmlns:env=\"http://www.w3.org/2003/05/soap-envelope\">env:Sender</env:Value>\n");
+        this.xml.append("         </soap:Code>\n");
+        this.xml.append("         <soap:Reason>\n");
+        this.xml.append("            <soap:Text xml:lang=\"pt-BR\">Uma ou mais regras negociais foram violadas, verifique a lista de erros.</soap:Text>\n");
+        this.xml.append("         </soap:Reason>\n");
+        this.xml.append("         <soap:Detail>\n");
+        this.xml.append("            <msf:MSFalha xmlns:msf=\"http://servicos.saude.gov.br/wsdl/mensageria/falha/v5r0/msfalha\">\n");
+        this.xml.append("               <msf:Mensagem xmlns:men=\"http://servicos.saude.gov.br/wsdl/mensageria/falha/v5r0/mensagem\">\n");
+        this.xml.append("                  <men:codigo>OSB_SEM_AUTENTICACAO</men:codigo>\n");
+        this.xml.append("                  <men:descricao>As credenciais informadas não são válidas</men:descricao>\n");
+        this.xml.append("               </msf:Mensagem>\n");
+        this.xml.append("            </msf:MSFalha>\n");
+        this.xml.append("         </soap:Detail>\n");
+        this.xml.append("      </soap:Fault>\n");
+        this.xml.append("   </soap:Body>\n");
+        this.xml.append("</soap:Envelope>");
     }
 
     /**
@@ -72,7 +62,7 @@ public class BsusTest {
      */
     @After
     public void tearDown() {
-        this.xml = "";
+        this.xml = new StringBuilder();
     }
 
     /**
@@ -84,16 +74,19 @@ public class BsusTest {
         bsus.setConnection(new Connection() {
             @Override
             public String consultarPosicaoEstoquePorCNES(int cnes) {
-                return xml;
+                return xml.toString();
             }
 
             @Override
-            public String consultarPosicaoEstoquePorCNESPrincipioAtivo(int cnes, String principio) {
+            public String consultarPosicaoEstoquePorCNESPrincipioAtivo(
+                    int cnes, String principio) {
                 return null;
             }
 
             @Override
-            public String consultarPosicaoEstoquePorCNESPrincipioAtivoPaginado(int cnes, String principio, int posicaoInicio, int qtdRegistrosPagina, int qtdRegistros) {
+            public String consultarPosicaoEstoquePorCNESPrincipioAtivoPaginado(
+                    int cnes, String principio, int posicaoInicio,
+                    int qtdRegistrosPagina, int qtdRegistros) {
                 return null;
             }
 
@@ -103,24 +96,35 @@ public class BsusTest {
             }
 
             @Override
-            public String consultarProdutoPorCNESDispensacaoPaginado(int cnes, int posicaoInicio, int qtdRegistrosPagina, int qtdRegistros) {
+            public String consultarProdutoPorCNESDispensacaoPaginado(int cnes,
+                    int posicaoInicio, int qtdRegistrosPagina,
+                    int qtdRegistros) {
                 return null;
             }
 
             @Override
             public void setCredential(String usuario, String senha) {
+                throw new UnsupportedOperationException(
+                        "Metétodo não utilizado.");
             }
 
             @Override
             public void setURL(String url) {
+                throw new UnsupportedOperationException(
+                        "Metétodo não utilizado.");
             }
 
             @Override
             public void setLog(Log log) {
+                throw new UnsupportedOperationException(
+                        "Método não utilizado.");
             }
+
         });
 
-        assertEquals(bsus.obterEstoquePorCNES(7604041), "Uma ou mais regras negociais foram violadas, verifique a lista de erros.\n"
+        assertEquals(bsus.obterEstoquePorCNES(7604041),
+                "Uma ou mais regras negociais foram violadas,"
+                + "verifique a lista de erros.\n"
                 + "OSB_SEM_AUTENTICACAO\n"
                 + "As credenciais informadas não são válidas");
     }
@@ -140,7 +144,7 @@ public class BsusTest {
             @Override
             public String consultarPosicaoEstoquePorCNESPrincipioAtivo(int cnes, String principio) {
 
-                return xml;
+                return xml.toString();
             }
 
             @Override
@@ -160,15 +164,22 @@ public class BsusTest {
 
             @Override
             public void setCredential(String usuario, String senha) {
+                throw new UnsupportedOperationException(
+                        "Metétodo não utilizado.");
             }
 
             @Override
             public void setURL(String url) {
+                throw new UnsupportedOperationException(
+                        "Metétodo não utilizado.");
             }
 
             @Override
             public void setLog(Log log) {
+                throw new UnsupportedOperationException(
+                        "Método não utilizado.");
             }
+
         });
 
         assertEquals(bsus.obterEstoquePorCNESEPrincipio(7604041, "PRINCIPIO"), "Uma ou mais regras negociais foram violadas, verifique a lista de erros.\n"
@@ -196,7 +207,7 @@ public class BsusTest {
             @Override
             public String consultarPosicaoEstoquePorCNESPrincipioAtivoPaginado(int cnes, String principio, int posicaoInicio, int qtdRegistrosPagina, int qtdRegistros) {
 
-                return xml;
+                return xml.toString();
             }
 
             @Override
@@ -211,14 +222,17 @@ public class BsusTest {
 
             @Override
             public void setCredential(String usuario, String senha) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
 
             @Override
             public void setURL(String url) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
 
             @Override
             public void setLog(Log log) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
         });
 
@@ -252,7 +266,7 @@ public class BsusTest {
             @Override
             public String consultarProdutoPorCNESDispensacao(int cnes) {
 
-                return xml;
+                return xml.toString();
             }
 
             @Override
@@ -262,15 +276,19 @@ public class BsusTest {
 
             @Override
             public void setCredential(String usuario, String senha) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
 
             @Override
             public void setURL(String url) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
 
             @Override
             public void setLog(Log log) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
+
         });
 
         assertEquals(bsus.obterDadosEEstoquePorCNES(7604041), "Uma ou mais regras negociais foram violadas, verifique a lista de erros.\n"
@@ -308,24 +326,41 @@ public class BsusTest {
             @Override
             public String consultarProdutoPorCNESDispensacaoPaginado(int cnes, int posicaoInicio, int qtdRegistrosPagina, int qtdRegistros) {
 
-                return xml;
+                return xml.toString();
             }
 
             @Override
             public void setCredential(String usuario, String senha) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
 
             @Override
             public void setURL(String url) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
 
             @Override
             public void setLog(Log log) {
+                throw new UnsupportedOperationException("Metétodo não utilizado.");
             }
+
         });
 
         assertEquals(bsus.obterDadosEEstoquePorCNESPaginado(7604041, 1111, 2222, 3333), "Uma ou mais regras negociais foram violadas, verifique a lista de erros.\n"
                 + "OSB_SEM_AUTENTICACAO\n"
                 + "As credenciais informadas não são válidas");
+    }
+}
+
+class TestLog implements Log {
+
+    @Override
+    public void info(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void erro(String message) {
+        System.err.println(message);
     }
 }
