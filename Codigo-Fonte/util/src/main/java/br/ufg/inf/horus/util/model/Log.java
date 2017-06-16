cf75da3f5b49452bf6115d1c3410ae346ae81d10
pedrodrim <pedrohenriquedrim@gmail.com>
2017-06-16 17:15:23 -0300

    Separado classes comuns em ambos os projetos em um projeto 'util'.

---
+++ Codigo-Fonte/util/src/main/java/br/ufg/inf/horus/util/model/Log.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.horus.util.model;
+
+/**
+ * Interface para geração de mensagens de controle (Log's).
+ *
+ * @author Pedro
+ */
+public interface Log {
+
+    /**
+     * Exibição de uma mensagem informativa.
+     *
+     * @param message Mensagem a ser exibida.
+     */
+    void info(String message);
+
+    /**
+     * Exibição de uma mensagem de erro.
+     *
+     * @param message Message a ser exibida.
+     */
+    void erro(String message);
+}
